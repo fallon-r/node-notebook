7133e67d1cb0b26041c366145667e20100a12ee1
rf-spuds <rfspuds@gmail.com>
2020-10-13 15:45:43 -0400

    Adding note basic logic

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
 
 const chalk = require("chalk");
+const yargs = require("yargs");
 const notes = require("./notes.js");
 
-const command = process.argv[2]
 
+// Add Command
+yargs.command({
+    command: 'add',
+    describe: "Add a new note",
+    builder:{
+        title:{
+            describe: "Note Title",
+            demandOption : true,
+            type: "string"
+        },
+        body: {
+            describe: "Note Body",
+            demandOption : true,
+            type: "string"
+        }
+    },
+    handler: function (argv){
+        console.log(chalk.green.bold("Title:") + chalk.green.inverse.bold(argv.title))
+        console.log(chalk.green.bold("Body:") + chalk.green.inverse.bold(argv.body))
+    }
+})
+// -------
+// Remove Command
+yargs.command({
+    command: 'remove',
+    describe: "Remove a note",
+    handler: function (){
+        console.log(chalk.red.bold.inverse("Removing a note..."))
+    }
+})
+// --------
+// List Command
+yargs.command({
+    command: 'list',
+    describe: "Lists available notes",
+    handler: function (){
+        console.log(chalk.yellowBright.bold.inverse("Here are all of your notes..."))
+    }
+})
+// -------
+// Read Command
+yargs.command({
+    command: 'read',
+    describe: "Read Notes",
+    handler: function (){
+        console.log(chalk.blueBright.bold.inverse("Reading your notes:"))
+    }
+})
+// -------
 
-if (command === 'add'){
-    console.log(chalk.greenBright.bold.inverse("Adding Note!"))
-} else if (command === "remove"){
-    console.log(chalk.redBright.bold.inverse("Removing note!"))
-}
+yargs.parse()
